c69c2ddb7a5269ece00e16ed20909094eb68139e
Azaz50 <azazmohammad14@gmail.com>
2024-07-11 11:38:57 +0530

    modify for responsive

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
         <p>As a skilled MERN stack developer, I specialize in building dynamic web applications using MongoDB, Express.js, React.js, and Node.js. With strong expertise in both front-end and back-end development, I create seamless, user-friendly experiences.</p>
         <div className="hero-action">
             <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
-            <div className="hero-resume"><a href='../../assets/Azaz_Mohammad_Resume.pdf' download="Azaz_Mohammad_Resume.pdf">My resume</a></div>
+            <div className="hero-resume"><a href='/Azaz_Mohammad_Resume.pdf' download="Azaz_Mohammad_Resume.pdf">My resume</a></div>
         </div>
       
     </div>
